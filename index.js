07fcfad140aacfec4a938e84069455c0de63b3d8
vips120 <vipulsingh0000@gmail.com>
2020-01-15 16:57:00 +0530

    express crud api

---
+++ index.js
@@ -0,0 +1,82 @@
+let express = require("express");
+let app = express();
+let port = process.env.NODE_ENV || 4800;
+let Joi = require("@hapi/joi");
+// console.log(app);
+app.use(express.json()); //in built-middleware
+let courses = [{ id: 1, name: "Angular" },
+{ id: 2, name: "Rxjs" },
+ { id: 3, name: "react native" },
+    { id: 4, name: " Javascripit " }
+];
+
+//fetch all data
+app.get("/api/courses", (req, res) => {
+    // res.send("hello user");
+    res.send(courses);
+});
+
+//fetch data by id
+app.get("/api/course/:id", (req, res) => {
+    // let id = req.params.id;
+    // res.send(id);
+    let course = courses.find(item => item.id === parseInt(req.params.id));
+    if (!course) { return res.status(404).send({ message: "Invalid course id" }) };
+    let { id, name } = course;
+    res.send(name);
+});
+
+
+//create a new course
+
+app.post("/api/course/newcourse", (req, res) => {
+    let { error } = ValidationError(req.body);
+    // console.log(result);
+    if(error) {return res.send(error.details[0].message)}
+
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(courses);
+});
+
+//DRY
+//update a course
+
+app.put("/api/course/updatecourse/:id", (req, res) => {
+    // id
+    let course = courses.find(item => item.id === parseInt(req.params.id));
+    if (!course) { return res.status(404).send({ message: "Invalid course id" }) };
+    //joi
+   
+    let { error } = ValidationError(req.body);
+    // console.log(result);
+    if (error) { return res.send(error.details[0].message) }
+    //save
+    course.name = req.body.name;
+    res.send(courses);
+});
+
+//REMOVE COURSE
+app.delete("/api/course/removecourse/:id", (req, res) => {
+      // id
+      let course = courses.find(item => item.id === parseInt(req.params.id));
+    if (!course) { return res.status(404).send({ message: "Invalid course id" }) };
+    let index = courses.indexOf(course); // 2
+    courses.splice(index, 1);
+    res.send({message: "Removed the data", c: courses});
+})
+
+function ValidationError(error) {
+    let Schema = Joi.object({
+        name: Joi.string().min(4).max(100).alphanum().required()
+    });
+    return Schema.validate(error);
+}
+
+
+
+
+app.listen(port, () => {console.log(`port working on ${port}`)})
